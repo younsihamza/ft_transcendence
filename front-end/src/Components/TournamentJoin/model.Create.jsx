7ca98267615a93b393be748de2c50f2163d1cbd6
younsihamza <younsihamza>
2024-07-06 21:30:25 +0100

    cc

--- front-end/src/Components/TournamentJoin/model.Create.jsx
+++ front-end/src/Components/TournamentJoin/model.Create.jsx
@@ -19,7 +19,7 @@ export default function ModelCreate({ setTournaments, setIsmodel }) {
         console.log(valuePlayers)
     }
     return (<div className='absolute top-0 bottom-0 left-0 right-0 bg-opacity-60 bg-secondaryColor z-10  flex justify-center items-center '>
-        <div className='opacity-100 bg-secondaryColor  py-5 px-5 flex flex-col justify-center items-center gap-8 border-2  border-forthColor rounded-lg max-w-[800px] min-w-[320px] '>
+        <form className='opacity-100 bg-secondaryColor  py-5 px-5 flex flex-col justify-center items-center gap-8 border-2  border-forthColor rounded-lg max-w-[800px] min-w-[320px]' >
             <h1 className="font-Valorax xsm:text-lg md:text-xl">CREATE NOW TOURNAMENT</h1>
             <div className='flex  h-[100%] w-[100%] gap-4 items-center flex-wrap justify-center'>
                 <div className='flex flex-col  w-[40%]  min-w-[250px] items-center '>
@@ -53,6 +53,6 @@ export default function ModelCreate({ setTournaments, setIsmodel }) {
                 <button className='border-2 h-[30px] w-[30%] rounded-lg border-forthColor font-bold hover:opacity-70' onClick={() => { setTournaments({ gameName: "PING PONG", players: valuePlayers, number: 0 }); setIsmodel(false) }}>CREATE</button>
                 <button className='border-2 w-[30%] h-[30px] rounded-lg border-forthColor font-bold hover:opacity-70' onClick={() => setIsmodel(false)}>CLOSE</button>
             </div>
-        </div>
+        </form>
     </div>)
 }
