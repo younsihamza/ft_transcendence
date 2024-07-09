d7c5c48eb83ee30f7eb18510f928e5d12c752316
younsihamza <younsihamza>
2024-07-09 09:47:23 +0100

    fix

--- front-end/src/Components/TournamentJoin/Friend.jsx
+++ front-end/src/Components/TournamentJoin/Friend.jsx
@@ -5,7 +5,7 @@ export default function Friend({ icon = false, gameName, hidden = false }) {
     const notify = () => toast.info("Send Match Request to Hyounsi", { theme: 'dark' });
     return (<div className="flex flex-row w-[100%] items-center">
         <img src="hyounsi.png" className={`rounded-full md:w-[60px] md:h-[60px] object-fit border-[2px] xsm:w-[60px] xsm:[60px] sm:w-[60px] sm:h-[60px] ${icon ? 'border-green-600' : 'border-red-600'}`} />
-        <div className={`ml-3 flex flex-row justify-between w-full items-center border-solid ${hidden ? "hidden" : ""} lg:flex w-[260px]`}>
+        <div className={`ml-3 flex flex-row justify-between  items-center border-solid ${hidden ? "hidden" : ""} lg:flex w-[260px]`}>
             <div className="max-w-[calc(100%-3rem)]">
                 <h3 className='font-medium text-ellipsis overflow-hidden whitespace-nowrap w-[100px]'>Hyounsi</h3>
                 <p className='text-xs opacity-70 text-ellipsis overflow-hidden whitespace-nowrap'>{icon ? "in lobby" : gameName}</p>
