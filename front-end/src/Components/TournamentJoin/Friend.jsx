e9215c0058690cb4e7383212dc137b5ff693cb3d
Hamza Younsi <younsihamza006@gmail.com>
2024-07-09 18:35:36 +0100

    dd

--- front-end/src/Components/TournamentJoin/Friend.jsx
+++ front-end/src/Components/TournamentJoin/Friend.jsx
@@ -1,8 +1,17 @@
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import 'ldrs/hourglass'
+import { useState } from 'react';
+
 export default function Friend({ icon = false, gameName, hidden = false,PlayerName }) {
-    const notify = () => toast.info("Send Match Request to Hyounsi", { theme: 'dark' });
+    const  [WaitRequest, setwaitRequest] = useState(false)
+    const notify = () => {toast.info("Send Match Request to Hyounsi", { theme: 'dark' })
+    setwaitRequest(true)
+    setTimeout(() => {
+        setwaitRequest(false)
+    }, 4000);
+}
     return (<div className="flex flex-row w-[100%] items-center">
         <img src="hyounsi.png" className={`rounded-full md:w-[60px] md:h-[60px] object-fit border-[2px] xsm:w-[60px] xsm:[60px] sm:w-[60px] sm:h-[60px] ${icon ? 'border-green-600' : 'border-red-600'}`} />
         <div className={`ml-3 flex flex-row justify-between  items-center border-solid ${hidden ? "hidden" : ""} lg:flex w-[260px]`}>
@@ -10,7 +19,7 @@ export default function Friend({ icon = false, gameName, hidden = false,PlayerNa
                 <h3 className='font-medium text-ellipsis overflow-hidden whitespace-nowrap w-[100px]'>{PlayerName}</h3>
                 <p className='text-xs opacity-70 text-ellipsis overflow-hidden whitespace-nowrap'>{icon ? "in lobby" : gameName}</p>
             </div>
-            {icon && <button onClick={notify}><img src="./png.png" className="w-5 h-5" /><ToastContainer autoClose={3000} /> </button>}
+            {icon && (!WaitRequest ? <button onClick={notify}><img src="./png.png" className="w-5 h-5" /><ToastContainer autoClose={3000} /> </button>: <l-hourglass size="40"bg-opacity="0.1" speed="1.75" color="white" ></l-hourglass>)}
         </div>
     </div>)
 }
