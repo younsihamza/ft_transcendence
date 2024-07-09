aa36b2e6e03ad7f7cef5f48c8efa8ef3ec37b448
younsihamza <younsihamza>
2024-07-09 17:24:43 +0100

    fix

--- front-end/src/Components/TournamentJoin/model.Create.jsx
+++ front-end/src/Components/TournamentJoin/model.Create.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function ModelCreate({ setTournaments, setIsmodel }) {
     const [value, setValue] = useState(1)
@@ -16,10 +18,30 @@ export default function ModelCreate({ setTournaments, setIsmodel }) {
     }
     const handlechangePlayer = (e) => {
         setValuePlayers(parseInt(e.target.value, 10))
-        console.log(valuePlayers)
     }
     const handleSubmit = (e) => {
         e.preventDefault()
+        const fd = new FormData(e.target)
+        const data = Object.fromEntries(fd.entries())
+        console.log(data)
+        const players = []
+        for(let item in data)
+        {
+            const name = data[item].trim()
+            if(name.length == 0)
+                return toast.error('the name of the players should not be empty or contain only spaces', {theme:'dark'})
+            players.push(name)
+        }
+        for(let i  = 0 ; i < players.length ; i++)
+            {
+                if(players.includes(players[i],i+1))
+                {
+                    toast.error('the name of the players should be deferent', {theme:'dark'})
+                    return
+                }
+            }
+        setTournaments({ gameName: value == 1 ? "ONLINE" : "OFFLINE", players: valuePlayers, number: 0, player: players });
+        setIsmodel(false)
     }
     return (<div className='absolute top-0 bottom-0 left-0 right-0 bg-opacity-60 bg-secondaryColor z-10  flex justify-center items-center '>
         <form className='opacity-100 bg-secondaryColor  py-5 px-5 flex flex-col justify-center items-center gap-8 border-2  border-forthColor rounded-lg max-w-[800px] min-w-[320px]'  onSubmit={handleSubmit}>
@@ -27,12 +49,11 @@ export default function ModelCreate({ setTournaments, setIsmodel }) {
             <div className='flex  h-[100%] w-[100%] gap-4 items-center flex-wrap justify-center'>
                 <div className='flex flex-col  w-[40%]  min-w-[250px] items-center '>
                     <label className='text-xl font-bold'>Game Mode:</label>
-                    <select value={value} onChange={handlechange} className='text-forthColor w-[80%] h-[30px] text-xl font-bold px-4 border-2 border-forthColor rounded-lg focus:outline-none'>
+                    <select value={value} onChange={handlechange} name="hamza" className='text-forthColor w-[80%] h-[30px] text-xl font-bold px-4 border-2 border-forthColor rounded-lg focus:outline-none'>
                         {optionGame.map((item, index) => (
                             <option value={item.value} key={index} className='text-forthColor   text-xl font-bold '>{item.label}</option>
                         ))}
                     </select>
-
                 </div>
                 <div className='flex flex-col w-[40%]  min-w-[250px] items-center'>
                     <label className='text-xl font-bold' >number of players:</label>
@@ -55,6 +76,7 @@ export default function ModelCreate({ setTournaments, setIsmodel }) {
             <div className='flex justify-evenly h-[100%] w-[100%]'>
                 <button className='border-2 h-[30px] w-[30%] rounded-lg border-forthColor font-bold hover:opacity-70' type='submit' >CREATE</button>
                 <button className='border-2 w-[30%] h-[30px] rounded-lg border-forthColor font-bold hover:opacity-70' onClick={() => setIsmodel(false)}>CLOSE</button>
+                <ToastContainer autoClose={3000}/>
             </div>
         </form>
     </div>)
--- front-end/src/Components/TournamentJoin/tournament.jsx
+++ front-end/src/Components/TournamentJoin/tournament.jsx
@@ -1,4 +1,4 @@
-export  default function Tournament({gameName,players})
+export  default function Tournament({gameName,players, mod})
 {
     return (<div className='flex  justify-between text-[1.5vw]'>
         <div className='border-2 flex justify-between w-[60%] h-[40px]  items-center rounded-xl px-2'>
