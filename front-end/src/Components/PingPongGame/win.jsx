ede84ce7137418ed2537f542e5e496f0c1e2ab08
Hamza Younsi <younsihamza006@gmail.com>
2024-07-12 20:06:09 +0100

    fix the problem win popup

--- front-end/src/Components/PingPongGame/win.jsx
+++ front-end/src/Components/PingPongGame/win.jsx
@@ -4,14 +4,34 @@ import { useState } from "react";
 
 export function Progress({value, max})
 {
-    
-const [progress, setProgress] = useState(0)
+    const [progress, setProgress] = useState(0)
+    const ref = useRef()
+    useEffect(()=>{
+        let status = null
+        status = setInterval(()=>{
+            setProgress((prevProgress)=> {
+                
+                if(prevProgress >= value)
+                {
+                    clearInterval(status)
+                    return value
+                }
+                return prevProgress + 1
+            })
+        },100)
+        return ()=> clearInterval(status)
+    },[])
 
-return (
+    useEffect(() => {
+        if (ref.current) {
+            ref.current.style.width = `${progress}%`;
+        }
+    }, [progress]);
+    return (
         <div className="w-full h-4 xsm:h-2 bg-white rounded-full">
-            <div className={`h-full w-[${progress}%] bg-thirdColor rounded-full`}></div>
+            <div className={`h-full transition-all duration-200 ease-in-out w-[${progress}%] bg-thirdColor rounded-full`} ref={ref}/> 
         </div> 
-)
+    )
 }
 
 export default function Win()
@@ -25,9 +45,17 @@ export default function Win()
                     <img src="ykhourba.jpeg" className="rounded-full w-[140px] border-[2px] border-forthColor object-cover" />
                     <p className="font-inter">YKHOURBA</p>
                     <p className="w-[100%] text-right">+10XP</p>
-                    <Progress value={64} max={100}/>
-                </div>
-                <div className="">
+                    <div className="w-[90%] font-inter text-xs bg-secondaryColor py-3 px-3 flex flex-col justify-center items-center rounded-full border-[1px] ">
+                        <div className="flex justify-between w-[100%]">
+                            <p>LEVEL 2</p>
+                            <p>LEVEL 3</p>
+                        </div>
+                        <Progress value={75} max={100}/>
+                    </div>
+                        <div className="w-[100%] flex justify-evenly">
+                            <button className="w-[30%] h-[40px] bg-secondaryColor font-inter border-[2px]">PLAY AGAIN</button>
+                            <button className="w-[30%] h-[40px] bg-secondaryColor font-inter border-[2px]">HOME</button>
+                        </div>
                 </div>
             </div>
 
